19d2bacde99943055ca9644f989c2154bb856b5d
Danish <danish@ultroid.tech>
2022-10-21 15:30:08 +0530

    Format with autopep8

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 """Example bot which execute python expression and returns a local execution environment which is enough to teach the concept"""
 
-from flask import Flask, request, json
-import traceback
-import sys
 import io
+import sys
+import traceback
+
+from flask import Flask, json, request
 
 app = Flask(__name__)
 
@@ -21,19 +22,19 @@ def _exec(code: str) -> str:
     return exc.strip() or stderr.strip() or stdout.strip() or "Success"
 
 
-@app.route('/', methods=['POST'])
+@app.route("/", methods=["POST"])
 def on_event():
     """Handles an event from Google Chat."""
     event = request.get_json()
-    if event['type'] == 'ADDED_TO_SPACE':
+    if event["type"] == "ADDED_TO_SPACE":
         text = f"Thanks for adding me to {event['space']['displayName']}!"
 
-    elif event['type'] == 'REMOVED_FROM_SPACE':
+    elif event["type"] == "REMOVED_FROM_SPACE":
         text = f"Bye Bye,nice tille stay at {event['space']['displayName']}!"
 
-    elif event['type'] == 'MESSAGE':
-        if event['message']['text'].startswith('@python exec'):
-            message = event['message']['text'].split('@python exec')
+    elif event["type"] == "MESSAGE":
+        if event["message"]["text"].startswith("@python exec"):
+            message = event["message"]["text"].split("@python exec")
             text = f"```{_exec(message[1][1:])}```"
 
-    return json.jsonify({'text': text})
+    return json.jsonify({"text": text})
